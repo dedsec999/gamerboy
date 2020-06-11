b060fd03a9183728632ba2e2488eb9351dd7c1f5
dedsec <59364307+dedsec999@users.noreply.github.com>
2020-06-11 14:53:02 +0500

    Update new.py

--- new.py
+++ new.py
@@ -81,8 +81,8 @@ print  """\033[1;91m=======================================
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "lover"
-CorrectPassword = "boy"
+CorrectUsername = "dedsec"
+CorrectPassword = "dedsec"
 
 loop = 'true'
 while (loop == 'true'):
